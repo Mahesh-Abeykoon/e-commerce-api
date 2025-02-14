e3f00a0ca1878281a58665deffdf090adf83d102
Mahesh-Abeykoon <mahesh@nimidev.com>
2025-02-14 22:18:07 +0530

    implement user routes

---
+++ src/routes/user-routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const userController = require('../controllers/user-controller');
+const auth = require('../middleware/auth');
+
+const router = express.Router();
+
+router.post('/register', userController.register);
+router.post('/login', userController.login);
+router.post('/logout', auth, userController.logout);
+router.post('/logoutAll', auth, userController.logoutAll);
+router.get('/profile', auth, userController.getProfile);
+
+
+module.exports = router;
+
